5350cdb7eecc945e004ddb801c4fe36d65ba0e41
Zion <zd819@ic.ac.uk>
2021-05-24 10:12:44 +0100

    server_v0 added

---
+++ static/server_v0.js
@@ -0,0 +1,13 @@
+
+// server.js
+ 
+const WebSocket = require('ws')
+ 
+const wss = new WebSocket.Server({ port: 8080 })
+ 
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('Hello! Message From Server!!')
+})
